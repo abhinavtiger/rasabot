f84af89d758123c5fcad33284eb4808fa9ac4a43
Abhinav Kumar <abhinav.kumar@tigeranalytics.local>
2023-05-01 17:52:37 +0530

    first commit

--- actions/actions.py
+++ actions/actions.py
@@ -1,56 +1,118 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+
+import json
+import time
+import datetime
+from typing import Text, Dict, List, Any, Optional
+from matplotlib.pyplot import text
+from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk import events
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import (  # noqa: 401
+    EventType,
+    SlotSet,
+    AllSlotsReset,
+    FollowupAction
+)
+
+class ActionUserMessage(Action):
+    def name(self) -> Text:
+        return "action_ask_greet"
+
+    def run(
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        dispatcher.utter_message(text="Hey! How are you?",
+                                 buttons=[
+                                     {"title": "Great", "payload": "/mood_great"},
+                                     {"title": "Sad", "payload": "/mood_unhappy"}
+                                 ])                     
+        return []
+
+class ActionUserWelcome(Action):
+    def name(self) -> Text:
+        return "action_welcome"
+
+    def run(
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        dispatcher.utter_message(text="Welcome To Registeration Portal",
+                                 buttons=[
+                                     {"title": "Register", "payload": "/affirm"},
+                                     {"title": "Not Interested", "payload": "/not_interest"}
+                                 ])
+        return []
 
 
-# This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-# from rasa_sdk.events import SlotSet, ConversationPaused, ConversationResumed, EventType
-# from rasa_sdk.forms import FormAction
-# import logging
-# from rasa_sdk.events import ReminderScheduled
+class ValidateNameForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_form_get_details"
 
+    def validate_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        if len(slot_value) <= 1:
+            dispatcher.utter_message(text="That's very short name.")
+            return {"name": None}
+        else:
+            return {"name": slot_value}
+class AskName(Action):
+    def name(self) -> Text:
+        return "action_ask_name"
 
-# class ActionUserMessage(Action):
-
-#     def name(self) -> Text:
-#         return "form_get_details"
-
-#     def run(
-#         self,slot_value: Any,dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict
-#     ) -> Dict[Text, Any]:
-
-#      return []
-        # required_slots = ["name", "number", "reason"]
-
-
-        # # All slots are filled.
-        # for i in required_slots:
-        #      return [SlotSet("requested_slot")]
-
-    # def name(self) -> Text:
-    #     return "form_get_details"
-
-    # @staticmethod
-    # def required_slots(tracker: Tracker) -> List[Text]:
-    #     """A list of required slots that the form has to fill."""
-         
-    #     return ["name", "number", "reason"]
-
-    # def submit(self,dispatcher, tracker: Tracker, domain: Dict[Text, Any],):
-    #     # dispatcher.utter_message(template="utter_details_thanks",
-    #     #                          Name=tracker.get_slot("name"),
-    #     #                          Mobile_number=tracker.get_slot("number"),
-    #     #                          Reason=tracker.get_slot("reason"))
-
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict
+    ) -> List[EventType]:
         
+        msg = "Hello! Could you please provide your name?"
+        dispatcher.utter_message(text=msg)
+        return []
 
-    #     return []
+class AskNumber(Action):
+    def name(self) -> Text:
+        return "action_ask_number"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict
+    ) -> List[EventType]:
+        
+        msg = "Could you please provide your contact number?"
+        dispatcher.utter_message(text=msg)
+        return []
+
+class AskReason(Action):
+    def name(self) -> Text:
+        return "action_ask_reason"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict
+    ) -> List[EventType]:
+        
+        msg = "Could you please provide the reason for joining this institution?"
+        dispatcher.utter_message(text=msg)
+        return []
+
+class SubmitRegistrationName(Action):
+    def name(self) -> Text:
+        return "action_submit_registration_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+    tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        Name = tracker.get_slot("name")
+        Mobile_number= tracker.get_slot("number")
+        Reason= tracker.get_slot("reason")
+        text=['Thanks for providing the given details your \nName: {} \nMobile_number: {} \nReason for placement: {}'.format(Name, Mobile_number,Reason)]
+        dispatcher.utter_message(text)
+
+        return []
+
+
     
 # class ActionSubmit(Action):
 #     def name(self) -> Text:
@@ -68,17 +130,3 @@
 #                                  Mobile_number=tracker.get_slot("number"),
 #                                  Reason=tracker.get_slot("reason"))
 
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
